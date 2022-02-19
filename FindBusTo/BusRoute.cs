3cf3051ee120c12f2db72bbe0513a32af137ab70
KrukRecon <adam.kruk00@gmail.com>
2022-02-19 22:22:21 +0100

    Learned how to used methods implemented by the Array class

---
+++ FindBusTo/BusRoute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pluralsight.ArraysCollections.Demos
+{
+	public class BusRoute
+	{
+		public int Number { get; }
+		public string Origin { get; }
+		public string Destination { get; }
+        public string[] PlacesServed { get; set; }
+        public BusRoute(int number, string[] placesServed)
+		{
+			this.Number = number;
+            this.PlacesServed = placesServed;
+			this.Origin = placesServed[0];
+			this.Destination = placesServed[^1];
+		}
+		public override string ToString() => $"{Number}: {Origin} -> {Destination}";
+		public bool Serves(string destination)
+        {
+			return Array.Exists(PlacesServed, place => place == destination);
+			//foreach (var place in PlacesServed)
+   //         {
+			//	if (place == destination)
+   //             {
+			//		return true;
+   //             }
+   //         }
+			//return false;
+        }
+	}
+}
---
+++ FindBusTo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pluralsight.ArraysCollections.Demos
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BusRoute[] allRoutes = BusRouteRepository.InitializeRoutes();
+
+			Console.WriteLine("Where do you want to go to?");
+			string location = Console.ReadLine();
+
+			BusRoute[] routes = FindBusTo(allRoutes, location);
+
+			if (routes.Length > 0)
+				foreach (var route in routes)
+                {
+					Console.WriteLine($"You can use route {route}");
+				}
+			else
+				Console.WriteLine($"No routes go to {location}");
+		}
+
+		public static BusRoute[] FindBusTo(BusRoute[] routes, string location)
+		{
+			return Array.FindAll(routes, route => route.Serves(location));
+			//foreach (BusRoute route in routes)
+			//{
+			//	if (route.Origin == location || route.Destination == location)
+			//		return route;
+			//}
+			//return null;
+		}
+	}
+}
