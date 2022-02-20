d35f1efdfc23c7829b02b1bb4eac4d2d94b562f2
KrukRecon <adam.kruk00@gmail.com>
2022-02-20 13:57:24 +0100

    Dictionariea

--- RemoveTestData/BusRouteRepository.cs
+++ RemoveTestData/BusRouteRepository.cs
@@ -8,16 +8,23 @@ namespace Pluralsight.ArraysCollections.Demos
 {
 	class BusRouteRepository
 	{
-		public static List<BusRoute> InitializeRoutes()
+		public static SortedDictionary<int, BusRoute> InitializeRoutes()
 		{
-            List<BusRoute> result = new();
-            result.Add(new BusRoute(40, new string[] { "Morecambe", "Lancaster", "Garstang", "Preston" }));
-			result.Add(new BusRoute(42, new string[] { "Lancaster", "Garstang", "Blackpool" }));
-			result.Add(new BusRoute(40, new string[] { "Morecambe", "Lancaster", "Garstang", "Preston" }));
-			result.Add(new BusRoute(5, new string[] { "Overton", "Morecambe", "Carnforth" }));
-			result.Add(new BusRoute(40, new string[] { "Morecambe", "Lancaster", "Garstang", "Preston" }));
-			result.Add(new BusRoute(555, new string[] { "Lancaster", "Carnforth", "Kendal", "Windermere", "Keswick" }));
-			return result;
+            BusRoute route40 = new(40, new string[] { "Morecambe", "Lancaster", "Garstang", "Preston" });
+			BusRoute route42 = new(42, new string[] { "Lancaster", "Garstang", "Blackpool" });
+			BusRoute route5 = new(5, new string[] { "Overton", "Morecambe", "Carnforth" });
+			BusRoute route555 = new(555, new string[] { "Lancaster", "Carnforth", "Kendal", "Windermere", "Keswick" });
+			BusRoute route100 = new(100, new string[] { "University", "Lancaster", "Morecambe" });
+
+            var routes = new SortedDictionary<int, BusRoute>
+            {
+                { 40, route40 },
+                { 42, route42 },
+                { 5, route5 },
+                { 555, route555 },
+                { 100, route100 }
+            };
+            return routes;
 		}
 	}
 
